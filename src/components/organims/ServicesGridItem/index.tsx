d822e03b09556759e9d01a471d5b66245222d98e
Difusão Web <difusaowebinc@gmail.com>
2022-09-14 16:41:57 -0300

    Updated styles ServiceCard.

--- src/components/organims/ServicesGridItem/index.tsx
+++ src/components/organims/ServicesGridItem/index.tsx
@@ -10,13 +10,12 @@ interface ServicesGridItemProps {
 export const ServicesGridItem = ({ item }: ServicesGridItemProps) => {
   return (
     <Grid item xs={6} md={3}>
-      <Box className={styles.gridItem}>
-        <Box className={styles.gridItemBackgound} />
+      <Box className={styles.card}>
         <Box className={styles.gridItemBox}>
           <Box className={styles.imagem}>
             <img className={styles.img} src={item.image} alt={item.title} />
           </Box>
-          <Typography className={styles.h3} variant="h5" component="h3">
+          <Typography className={styles.h4} variant="h4" component="p">
             {item.title}
           </Typography>
 
@@ -24,9 +23,7 @@ export const ServicesGridItem = ({ item }: ServicesGridItemProps) => {
             {item.list.map((operation, index) => (
               <li key={index} className={styles.li}>
                 <Typography
-                  variant="subtitle1"
                   component="p"
-                  className={styles.p}
                 >
                   {operation}
                 </Typography>
