15ecf129f9f5849b0e4e4728f33f46604a104738
Difusão Web <difusaowebinc@gmail.com>
2022-09-14 16:43:02 -0300

    Updated db Services.

--- src/components/ecosystems/Services/db.ts
+++ src/components/ecosystems/Services/db.ts
@@ -1,10 +1,10 @@
-import DesenvolvimentoAplicativoAndroidImage from '../../../assets/images/services/desenvolvimento-de-aplicativo-android.png?width=300&ts-ignore'
-import DesenvolvimentoAplicativoIOSImage from '../../../assets/images/services/desenvolvimento-de-aplicativo-ios.png?width=300&ts-ignore'
-import DesenvolvimentoLojasVirtuaisImage from '../../../assets/images/services/desenvolvimento-de-lojas-virtuais.png?width=300&ts-ignore'
-import DesenvolvimentoLojasVirtuaisWoocommerceImage from '../../../assets/images/services/desenvolvimento-de-lojas-virtuais-woocommerce.png?width=300&ts-ignore'
-import DesenvolvimentoSistemasWebImage from '../../../assets/images/services/desenvolvimento-de-sistemas-web.png?width=300&ts-ignore'
-import DesenvolvimentoBlogsImage from '../../../assets/images/services/desenvolvimento-de-blogs.png?width=300&ts-ignore'
-import DesenvolvimentoLandingPagesImage from '../../../assets/images/services/desenvolvimento-de-landing-pages.png?width=300&ts-ignore'
+import DesenvolvimentoSistemasWebImageUrl from '../../../assets/images/services/desenvolvimento-de-sistemas-web.png?width=300&ts-ignore'
+import DesenvolvimentoAplicativoAndroidImageUrl from '../../../assets/images/services/desenvolvimento-de-aplicativo-android.png?width=300&ts-ignore'
+import DesenvolvimentoLojasVirtuaisImageUrl from '../../../assets/images/services/desenvolvimento-de-lojas-virtuais.png?width=300&ts-ignore'
+import DesenvolvimentoBlogsImageUrl from '../../../assets/images/services/desenvolvimento-de-blogs.png?width=300&ts-ignore'
+import DesenvolvimentoLojasVirtuaisWoocommerceImageUrl from '../../../assets/images/services/desenvolvimento-de-lojas-virtuais-woocommerce.png?width=300&ts-ignore'
+import DesenvolvimentoBlogsWordpressImageUrl from '../../../assets/images/services/desenvolvimento-de-blogs-wordpress.png?width=300&ts-ignore'
+import DesenvolvimentoLandingPagesImageUrl from '../../../assets/images/services/desenvolvimento-de-landing-pages.png?width=300&ts-ignore'
 
 export interface CardInterface {
   image: string
@@ -13,43 +13,7 @@ export interface CardInterface {
 }
 export const db: CardInterface[] = [
   {
-    image: DesenvolvimentoAplicativoAndroidImage.src,
-    title: 'Desenvolvimento de Aplicativo Android',
-    list: [
-      'Aplicativos institucionais.',
-      'Aplicativos de gerencia de negócios.',
-      'Aplicativos de vendas de produtos ou serviços.'
-    ]
-  },
-  {
-    image: DesenvolvimentoAplicativoIOSImage.src,
-    title: 'Desenvolvimento de Aplicativo IOS',
-    list: [
-      'Aplicativos institucionais.',
-      'Aplicativos de gerencia de negócios.',
-      'Aplicativos de vendas de produtos ou serviços.'
-    ]
-  },
-  {
-    image: DesenvolvimentoLojasVirtuaisImage.src,
-    title: 'Desenvolvimento de Lojas Virtuais',
-    list: [
-      'Funcionalidades únicas.',
-      'Carregamento rápido.',
-      'Desing especifico para seu negócio.'
-    ]
-  },
-  {
-    image: DesenvolvimentoLojasVirtuaisWoocommerceImage.src,
-    title: 'Desenvolvimento de Lojas Virtuais Woocommerce',
-    list: [
-      'Lojas simples.',
-      'Plataforma Woocommerce + Wordpress.',
-      'Diversos métodos de pagamentos.'
-    ]
-  },
-  {
-    image: DesenvolvimentoSistemasWebImage.src,
+    image: DesenvolvimentoSistemasWebImageUrl,
     title: 'Desenvolvimento de Sistemas Web',
     list: [
       'Redução de trabalho manual.',
@@ -58,7 +22,25 @@ export const db: CardInterface[] = [
     ]
   },
   {
-    image: DesenvolvimentoBlogsImage.src,
+    image: DesenvolvimentoAplicativoAndroidImageUrl,
+    title: 'Desenvolvimento de Aplicativo Android',
+    list: [
+      'Aplicativos institucionais.',
+      'Aplicativos de gerencia de negócios.',
+      'Aplicativos de vendas de produtos ou serviços.'
+    ]
+  },
+  {
+    image: DesenvolvimentoLojasVirtuaisImageUrl,
+    title: 'Desenvolvimento de Lojas Virtuais',
+    list: [
+      'Funcionalidades únicas.',
+      'Carregamento rápido.',
+      'Desing especifico para seu negócio.'
+    ]
+  },
+  {
+    image: DesenvolvimentoBlogsImageUrl,
     title: 'Desenvolvimento de Blogs',
     list: [
       'Melhore a brand de sua marca.',
@@ -67,7 +49,23 @@ export const db: CardInterface[] = [
     ]
   },
   {
-    image: DesenvolvimentoLandingPagesImage.src,
+    image: DesenvolvimentoLojasVirtuaisWoocommerceImageUrl,
+    title: 'Desenvolvimento de Lojas Virtuais Woocommerce',
+    list: [
+      'Lojas simples.',
+      'Plataforma Woocommerce + Wordpress.',
+      'Diversos métodos de pagamentos.'
+    ]
+  },
+  {
+    image: DesenvolvimentoBlogsWordpressImageUrl,
+    title: 'Desenvolvimento de Blogs Wordpress',
+    list: [
+      'Utilize um CMS popular.'
+    ]
+  },
+  {
+    image: DesenvolvimentoLandingPagesImageUrl,
     title: 'Desenvolvimento de Landing Pages',
     list: [
       'Anuncie seus serviços ou produtos',
