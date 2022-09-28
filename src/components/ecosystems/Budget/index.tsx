fdc8cafac759dc3acfc30cc070537d73a2fd4d15
Difusão Web <difusaowebinc@gmail.com>
2022-09-28 08:08:06 -0300

    Buget component has been updated.

--- src/components/ecosystems/Budget/index.tsx
+++ src/components/ecosystems/Budget/index.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react'
 import { Container, Box, Stack, Typography } from '@mui/material'
 
-import {Button} from '../../atoms/Button'
+import { Button } from '../../atoms/Button'
 import { contacts } from '../../../store/contacts'
 import styles from './index.module.scss'
 
@@ -9,14 +9,15 @@ export const Budget: React.FC = () => {
   return (
     <Box className={styles.section} component="section">
       <Container maxWidth="lg" className={styles.container}>
-        <Typography
-          component="h2"
-          variant="h2"
-          className={styles.h2}
-        >
+        <Typography component="h2" variant="h2">
           Solicite seu orçamento
         </Typography>
-        <Button content="Whatsapp" href={contacts.whatsapp} size="large" className={styles.button} />
+        <Button
+          content="Whatsapp"
+          href={contacts.whatsapp}
+          size="large"
+          className={styles.button}
+        />
       </Container>
     </Box>
   )
