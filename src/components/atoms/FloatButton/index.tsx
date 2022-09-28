a7458c6ebf47edf75f6a791497752ab673199ded
Difusão Web <difusaowebinc@gmail.com>
2022-09-28 07:58:38 -0300

    FloatButton component and styles has been updated.

--- src/components/atoms/FloatButton/index.tsx
+++ src/components/atoms/FloatButton/index.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react'
 import { Fab } from '@mui/material'
-import WhatsAppIcon from '@mui/icons-material/WhatsApp'
+import EmailIcon from '@mui/icons-material/Email'
 
 import { contacts } from '../../../store/contacts'
 import style from './index.module.scss'
@@ -9,11 +9,11 @@ export const FloatButton: React.FC = () => {
   return (
     <Fab
       color="primary"
-      aria-label="add"
+      aria-label="Contato"
       className={style.floatButton}
-      href={contacts.whatsapp}
+      href={contacts.eMail.link}
     >
-      <WhatsAppIcon className={style.icon} />
+      <EmailIcon className={style.icon} />
     </Fab>
   )
 }
