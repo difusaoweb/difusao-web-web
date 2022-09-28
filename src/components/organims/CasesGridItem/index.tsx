43074817af76b20b690cdbfa41e8ea277ada4b17
Difusão Web <difusaowebinc@gmail.com>
2022-09-28 08:00:19 -0300

    CasesGridItem component and styles has been updated.

--- src/components/organims/CasesGridItem/index.tsx
+++ src/components/organims/CasesGridItem/index.tsx
@@ -1,9 +1,10 @@
 import * as React from 'react'
-import { Typography, Grid, Box, Card } from '@mui/material'
+import { Typography, Grid, Box } from '@mui/material'
 import Carousel from 'react-material-ui-carousel'
 import { isMobile } from 'react-device-detect'
 
 import { CardInterface } from '../../ecosystems/Cases/db'
+import { Card } from '../../atoms/Card'
 import styles from './index.module.scss'
 
 interface CasesGridItemProps {
@@ -12,34 +13,29 @@ interface CasesGridItemProps {
 export const CasesGridItem = ({ item }: CasesGridItemProps) => {
   return (
     <Grid item xs={6} md={3}>
-      <Card className={styles.gridItem} raised>
-        <Box className={styles.gridItemBox}>
-          <Carousel
-            autoPlay={true}
-            animation="slide"
-            duration={300}
-            navButtonsAlwaysVisible={isMobile}
-            className={styles.carousel}
-          >
-            {item.imagens.map((image, index) => (
-              <Box key={index} className={styles.imagemBox}>
-                <img
-                  className={styles.img}
-                  src={image}
-                  alt={`${item.title}, ${item.description}-${index}`}
-                />
-              </Box>
-            ))}
-          </Carousel>
-          <Box className={styles.texts}>
-            <Typography className={styles.h3} variant="h5" component="h3">
-              {item.title}
-            </Typography>
-            <Typography className={styles.p} variant="subtitle1" component="p">
-              {item.description}
-            </Typography>
-          </Box>
-        </Box>
+      <Card>
+        <Carousel
+          autoPlay={true}
+          animation="slide"
+          duration={300}
+          navButtonsAlwaysVisible={isMobile}
+          className={styles.carousel}
+          activeIndicatorIconButtonProps={{ className: styles.indicatorActive }}
+        >
+          {item.imagens.map((image, index) => (
+            <Box key={index} className={styles.imagemBox}>
+              <img
+                className={styles.img}
+                src={image}
+                alt={`${item.title}, ${item.description}-${index}`}
+              />
+            </Box>
+          ))}
+        </Carousel>
+        <Typography className={styles.h3} variant="h4" component="h3">
+          {item.title}
+        </Typography>
+        <Typography component="p">{item.description}</Typography>
       </Card>
     </Grid>
   )
