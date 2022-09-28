76635b442913a5e7c1e23f07bc78b6a6ebb7a4f5
Difusão Web <difusaowebinc@gmail.com>
2022-09-28 07:59:29 -0300

    ServicesGridItem component and styles has been updated.

--- src/components/organims/ServicesGridItem/index.tsx
+++ src/components/organims/ServicesGridItem/index.tsx
@@ -3,6 +3,7 @@ import { Typography, Box, Grid } from '@mui/material'
 
 import styles from './index.module.scss'
 import { CardInterface } from '../../ecosystems/Services/db'
+import { Card } from '../../atoms/Card'
 
 interface ServicesGridItemProps {
   item: CardInterface
@@ -10,28 +11,21 @@ interface ServicesGridItemProps {
 export const ServicesGridItem = ({ item }: ServicesGridItemProps) => {
   return (
     <Grid item xs={6} md={3}>
-      <Box className={styles.card}>
-        <Box className={styles.gridItemBox}>
-          <Box className={styles.imagem}>
-            <img className={styles.img} src={item.image} alt={item.title} />
-          </Box>
-          <Typography className={styles.h4} variant="h4" component="h3">
-            {item.title}
-          </Typography>
-
-          <ul className={styles.ul}>
-            {item.list.map((operation, index) => (
-              <li key={index} className={styles.li}>
-                <Typography
-                  component="p"
-                >
-                  {operation}
-                </Typography>
-              </li>
-            ))}
-          </ul>
+      <Card>
+        <Box className={styles.imagem}>
+          <img className={styles.img} src={item.image} alt={item.title} />
         </Box>
-      </Box>
+        <Typography className={styles.h3} variant="h4" component="h3">
+          {item.title}
+        </Typography>
+        <ul>
+          {item.list.map((operation, index) => (
+            <li key={index}>
+              <Typography component="p">{operation}</Typography>
+            </li>
+          ))}
+        </ul>
+      </Card>
     </Grid>
   )
 }
