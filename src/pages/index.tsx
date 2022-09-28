2f194fd35c3538ba811c067a9695c2ac20dca27a
Difusão Web <difusaowebinc@gmail.com>
2022-09-28 08:16:36 -0300

    Home page has been updated.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,12 +2,12 @@ import * as React from 'react'
 
 import { HeaderBar } from '../components/ecosystems/Header'
 import { Hero } from '../components/ecosystems/Hero'
-import {Services} from '../components/ecosystems/Services'
-import {Budget} from '../components/ecosystems/Budget'
-import {Cases} from '../components/ecosystems/Cases'
-import {Clients} from '../components/ecosystems/Clients'
-import {About} from '../components/ecosystems/About'
-import {Contact} from '../components/ecosystems/Contact'
+import { Services } from '../components/ecosystems/Services'
+import { Budget } from '../components/ecosystems/Budget'
+import { Cases } from '../components/ecosystems/Cases'
+import { Clients } from '../components/ecosystems/Clients'
+import { About } from '../components/ecosystems/About'
+import { Contact } from '../components/ecosystems/Contact'
 import { Footer } from '../components/ecosystems/Footer'
 import { FloatButton } from '../components/atoms/FloatButton'
 
@@ -20,9 +20,11 @@ export const Home = () => {
         <Services />
         <Budget />
         <Cases />
+        <Budget />
         <Clients />
         <Budget />
         <About />
+        <Budget />
         <Contact />
       </main>
       <Footer />
