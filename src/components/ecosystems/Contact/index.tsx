599a95a4209ba87602983030738ebb960e451d13
Difusão Web <difusaowebinc@gmail.com>
2022-09-28 10:48:21 -0300

    Contact component and styles has been updated.

--- src/components/ecosystems/Contact/index.tsx
+++ src/components/ecosystems/Contact/index.tsx
@@ -19,7 +19,7 @@ export const Contact: React.FC = () => {
           <Typography variant="h2" component="h2">
             Contato
           </Typography>
-          <Grid container spacing={3}>
+          <Grid container spacing={3} className={styles.grid}>
             {cards.map((contact, index) => (
               <Grid key={index} xs={4} md={2}>
                 <Card>
