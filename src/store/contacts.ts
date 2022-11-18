ecf90671b171463586442a48bc8f556c0a633a5a
Difusão Web <difusaowebinc@gmail.com>
2022-11-18 08:42:47 -0300

    Contact email has been replaced.

--- src/store/contacts.ts
+++ src/store/contacts.ts
@@ -1,5 +1,5 @@
 export const contacts = {
-  eMail: { title: 'e-Mail', link: 'mailto:difusaoweb@proton.me' },
+  eMail: { title: 'e-Mail', link: 'mailto:contact@difusaoweb.com' },
   telegram: { title: 'Telegram', link: 'https://t.me/difusaoweb' },
   whatsapp: { title: 'WhatsApp', link: 'https://wa.link/dqf8zh' },
   linkedin: {
